8ad358c6e308f7703736dfdc0f6783bf1b488b3e
kfirkoren <kfirmail@gmail.com>
2023-11-05 14:42:04 +0200

    RootNavigation.js clear

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -4,8 +4,6 @@ import {Authenticated, NonAuthenticated} from './MainNavigation';
 
 const RootNavigation = () => {
   const user = useSelector(state => state.user);
-  //ברגע שהיוזר יתנתק אוטומטית יופיע לו המסך של הלוג אין כי הנביגייטור ייקלוט שהבדיקה
-  //פה של יוזר לוג אין הפכה לפולס ולא טרו (לשקר ולא אמת)
   return user.isLoggedIn ? <Authenticated /> : <NonAuthenticated />;
 };
 
