294e3ccde62a98bd95ff97d9c3883c5e46bc243a
kfirkoren <kfirmail@gmail.com>
2023-11-05 14:48:32 +0200

    store.js clear

--- redux/store.js
+++ redux/store.js
@@ -6,8 +6,6 @@ import {logger} from 'redux-logger/src';
 import Categories from './reducers/Categories';
 import Donations from './reducers/Donations';
 
-//פה נגדיר בעצם באיזה רידוסרס(אובייקטים) אנחנו נרצה שיהיו לנו בחנות ושתהייה
-//לנו בעצם גישה אליהם מכל מקום בפרוקייט
 const rootReducer = combineReducers({
   user: User,
   categories: Categories,
@@ -20,8 +18,6 @@ const configuration = {
   version: 1,
 };
 
-//זה בעצם מאחסן את כל האבוייקטים שיצרנו בתוך מחסן ששומר על הערכים של האבוייקטים
-//ככה שלאחר שנטען את האפליקציה מחדש הערכים ששינינו יישמרו ולא יאבדו
 const persistedReducer = persistReducer(configuration, rootReducer);
 
 const store = configureStore({
@@ -32,16 +28,7 @@ const store = configureStore({
     });
   },
 
-  //ההבדל בין הקוד העליון לתחתות זה שהעליו מדפיס את המצב לפני ואחרי לקונדול לוג
-  //והעליון לא מדפיס
-  // middleware: getDefaultMiddleware => {
-  //   return getDefaultMiddleware({
-  //     serializableCheck: false,
-  //   }).concat(logger);
-  // },
 });
 
 export default store;
-//אובייקט שאחראי להעלות את כל הנתונים ששמרנו במחסן בצורה טובה כאשר עשינו ריסטור לאפליקציה
-//לא בדיוק הבנתי את המשמעות של זה יכול להיות שמה שכתוב למעלה זה חירטוט
 export const persistor = persistStore(store);
