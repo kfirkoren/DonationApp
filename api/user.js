eae56714aee750c55c7ce4d6dc7e6c8389f28e70
kfirkoren <kfirmail@gmail.com>
2023-11-05 14:34:43 +0200

    user.js clear

--- api/user.js
+++ api/user.js
@@ -3,14 +3,10 @@ import {err} from 'react-native-svg/lib/typescript/xml';
 import store from '../redux/store';
 import {updateToken} from '../redux/reducers/User';
 
-//אסור לשכוח את כל האוויטים (את כל הלחכות) אחרת התהליך לא ייקרה כמו שצריך !
 export const createUser = async (fullName, email, password) => {
   try {
     const user = await auth().createUserWithEmailAndPassword(email, password);
-    //אחת מהתכונות שיש ליוזר בפיירבייס זה דיספלי ניים
-    //ואפשר באמצעות תכונה זו לשדך ליוזר את השם המלא שלו
     await user.user.updateProfile({displayName: fullName});
-    // console.log(user);
     return user;
   } catch (error) {
     if (error.code === 'auth/email-already-in-use') {
@@ -44,7 +40,6 @@ export const loginUser = async (email, password) => {
           'The email you entered does not exist. Please create a new account',
       };
     }
-    //אם נרצה לדעת באיזה תקלות אפשר להתקל כדי להדפיס להם דברים מתאימים נוכל להדפיס את התקלות
     console.log(error);
     return {
       status: false,
@@ -54,11 +49,9 @@ export const loginUser = async (email, password) => {
 };
 
 export const logOut = async () => {
-  //אווס אחראי על היוזר המחובר אז הוא יודע גם את מי לנתק
   await auth().signOut();
 };
 
-// פוקציה שמעדכנת ביוזר שברידקס את הטוקן ובנוסף גם נותנת טוקן עדכני
 export const checkToken = async () => {
   try {
     let response = await auth().currentUser.getIdToken(true);
