5ff449f152ad827c011034d6233cf6474b1c0eb8
kfirkoren <kfirmail@gmail.com>
2023-11-05 14:38:01 +0200

    style.js clear

--- assets/styles/globalStyle.js
+++ assets/styles/globalStyle.js
@@ -5,11 +5,9 @@ import {horizontalScale, scaleFontSize, verticalScale} from './scaling';
 const globalStyle = StyleSheet.create({
   backgroundWhite: {
     backgroundColor: 'white',
-    //נותן רקע לבן לדף
   },
   flex: {
     flex: 1,
-    //דואג שהרקע הלבן שנתנו לדף יהייה לכל העמוד ולא רק למעלה
   },
   flexGrow: {
     flexGrow: 1,
